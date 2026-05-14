c43c31b0d2f5e34e0402753d3ed8bffb9a732b49
P. D. T. Neranjaka <teshanpanagoda173@gmail.com>
2026-05-14 22:34:51 +0530

    implement stack initialization of stack, stack overflow and underflow methods

--- calculator.c
+++ calculator.c
@@ -8,3 +8,18 @@ typedef struct {
     double data[MAX_STACK_SIZE];
     int    top;               
 } Stack;
+
+// initialize the stack
+void initialize_stack(Stack *s) {
+    s->top = -1;
+}
+
+// check if the stack is empty
+int is_empty(Stack *s) {
+    return s->top == -1;
+}
+
+// check if the stack is full
+int is_full(Stack *s){
+    return s->top == MAX_STACK_SIZE - 1;
+}
